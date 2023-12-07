4a2eb6d4efa57985426dea712aa3b9a7728f3996
Leonard Cohn <leonardacohn@gmail.com>
2023-12-06 20:20:12 -0600

    Initial commit and day 1

---
+++ src/day_1/part_1.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+fs.readFile('src/day_1/in.txt', (err, data) => {
+
+  if (err) throw err
+
+  const lines = data.toString().split('\n')
+
+  let total = 0
+
+  for (let line of lines) {
+
+    let num = ''
+
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] >= '0' && line[i] <= '9') {
+        num += line[i]
+        break
+      }
+    }
+
+    for (let i = line.length - 1; i >= 0; i--) {
+      if (line[i] >= '0' && line[i] <= '9') {
+        num += line[i]
+        break
+      }
+    }
+    
+    total += Number(num)
+
+  }
+
+  console.log(total)
+
+})
---
+++ src/day_1/part_2.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+fs.readFile('src/day_1/in.txt', (err, data) => {
+
+  if (err) throw err
+
+  const lines = data.toString().split('\n')
+  const spelled = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ]
+
+  let total = 0
+
+  for (let line of lines) {
+
+    let num = ''
+
+    let found = false
+    for (let i = 0; i < line.length; i++) {
+      if (!found) {
+        if (line[i] >= '0' && line[i] <= '9') {
+          num += line[i]
+          found = true
+        } else {
+          for (let spell of spelled) {
+            if (line.indexOf(spell) == i) {
+              num += spelled.indexOf(spell) + 1
+              found = true
+            }
+          }
+        }
+      }
+    }
+
+    found = false
+    for (let i = line.length - 1; i >= 0; i--) {
+      if (!found) {
+        if (line[i] >= '0' && line[i] <= '9') {
+          num += line[i]
+          found = true
+        } else {
+          for (let spell of spelled) {
+            if (line.indexOf(spell, i) == i) {
+              num += spelled.indexOf(spell) + 1
+              found = true
+            }
+          }
+        }
+      }
+    }
+    
+    total += Number(num)
+
+  }
+
+  console.log(total)
+
+})
